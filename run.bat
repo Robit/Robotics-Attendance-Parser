0a7b1e406960883903500af9d05cac186d5e192d
RohitM2019 <rohitmittal@sfhs.com>
2018-01-05 18:18:14 -0800

    Renamed batch file to run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 java -cp AttendanceParser.jar AttendanceParser "data.csv" "G:\Team Drives\Lancer Robotics   Board\Attendance\attendance.csv"
 java -cp AttendanceParser.jar FileCopyUtil "data.csv" "G:\Team Drives\Lancer Robotics   Board\Attendance\data.csv"
+PAUSE
